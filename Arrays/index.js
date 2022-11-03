75fa141ba6c3cff8fb586260e03b2ae2169ab31d
ashokkumarga <ashokkumarga0@gmail.com>
2022-11-03 22:34:38 +0530

    arrays

---
+++ Arrays/index.js
@@ -0,0 +1,10 @@
+let size = ['S', 'M', 'L', 'XL']
+let names = ["ashok", "harish",  "naveen", "sarjith"]
+//index       0        1            2       3
+console.log(names)
+//read array elemetnts 
+console.log(names[0])
+console.log(names[1])
+console.log(names[2])
+console.log(names[3])
+console.log(names[30])
---
+++ Arrays/one.js
@@ -0,0 +1,15 @@
+let number=[1,2,3,4,5]
+for(i=0;i<=4;i++){
+    console.log(number[i])
+}
+
+let names=["ashok","dinesh","anji","anju"]
+for(i=0;i<=3;i++){
+    console.log(names[i])      
+}
+
+
+let numbers=[1,2,3,4,5,6]
+for(i=5;i>=0;i--){
+    console.log(numbers[i])
+}
