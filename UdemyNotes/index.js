0bd188c83e8b15e2bfc554b9e8304b084a21fd07
ashokkumarga <ashokkumarga0@gmail.com>
2022-11-11 14:59:10 +0530

    practice

---
+++ UdemyNotes/index.js
@@ -0,0 +1,25 @@
+// var tweet = prompt("Compose your Tweet Here:");
+// var tweetCount =tweet.length;
+// // alert("You have enterd the characters " + tweetCount + "Charaters," +" And you have " + (150-tweetCount) + "Remaining")
+// alert (tweet.slice(0,140))
+
+
+// var A=["AK","BC","MP","BV","T"]
+// //       0    1   2    3    4
+
+// console.log(A);
+// var b=A.slice(1,4);
+// console.log(b);
+ 
+// var D="Ashok";
+// console.log(D.slice(0,1))
+
+
+// var A = "ashok";
+
+// console.log(A.slice(0,1))
+
+
+var A=["AK","BC","MP","BV","T"]
+
+console.log(A.slice(2,3))
